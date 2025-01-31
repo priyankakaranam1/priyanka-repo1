13662d7c829be9b0d8aa7a42a6f2416e2eb05f99
priyankakaranam1 <l.priyakaranam@gmail.com>
2025-01-31 14:33:55 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -5,7 +5,7 @@ provider "aws" {
 }
 
 resource "aws_vpc" "test-vpc" {
-    cidr_block = "10.0.0.0/16"
+    cidr_block = "10.0.0.0/16"   
     instance_tenancy = "default" 
     tags = {
         name = "dev-vpc"
